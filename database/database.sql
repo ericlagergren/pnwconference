9fb3cb1115f75da886c9bbad88063532f0b0bc9b
Eric <ericscottlagergren@gmail.com>
2015-07-08 23:10:57 -0700

    more debug... who overwrote!

--- database/database.sql
+++ database/database.sql
@@ -29,48 +29,6 @@ SET default_tablespace = '';
 SET default_with_oids = false;
 
 --
--- Name: failed_logins; Type: TABLE; Schema: public; Owner: postgres; Tablespace: 
---
-
-CREATE TABLE failed_logins (
-	ip inet NOT NULL,
-	username text NOT NULL,
-	attempts integer NOT NULL,
-	last timestamp with time zone NOT NULL
-);
-
-
-ALTER TABLE failed_logins OWNER TO postgres;
-
---
--- Name: hotp_codes; Type: TABLE; Schema: public; Owner: postgres; Tablespace: 
---
-
-CREATE TABLE hotp_codes (
-	email text NOT NULL,
-	otp bytea,
-	qr bytea
-);
-
-
-ALTER TABLE hotp_codes OWNER TO postgres;
-
---
--- Name: jobs; Type: TABLE; Schema: public; Owner: postgres; Tablespace: 
---
-
-CREATE TABLE jobs (
-	job_id text NOT NULL,
-	user_id text NOT NULL,
-	job_type integer NOT NULL,
-	start_date date NOT NULL,
-	end_date date
-);
-
-
-ALTER TABLE jobs OWNER TO postgres;
-
---
 -- Name: sessions; Type: TABLE; Schema: public; Owner: postgres; Tablespace: 
 --
 
@@ -79,7 +37,7 @@ CREATE TABLE sessions (
 	auth_token bytea NOT NULL,
 	csrf_token bytea NOT NULL,
 	email text NOT NULL,
-	org text NOT NULL,
+	school text NOT NULL,
 	date bigint NOT NULL
 );
 
@@ -92,67 +50,55 @@ ALTER TABLE sessions OWNER TO postgres;
 
 CREATE TABLE users (
 	email text NOT NULL,
-	org text NOT NULL,
+	school text NOT NULL,
 	username text NOT NULL,
-	password text NOT NULL,
-	tfa boolean DEFAULT false NOT NULL,
-	user_data xml
+	password text NOT NULL
 );
 
 
 ALTER TABLE users OWNER TO postgres;
 
 --
--- Name: orgs; Type: TABLE; Schema: public; Owner: postgres; Tablespace: 
+-- Name: schools; Type: TABLE; Schema: public; Owner: postgres; Tablespace: 
 --
 
-CREATE TABLE orgs (
-	org text NOT NULL,
-	prefix text NOT NULL
+CREATE TABLE schools (
+	school text NOT NULL
 );
 
 
-ALTER TABLE orgs OWNER TO postgres;
+ALTER TABLE schools OWNER TO postgres;
+
+--
+-- Name: signups; Type: TABLE; Schema: public; Owner: postgres; Tablespace: 
+--
+
+CREATE TABLE signups (
+	email text NOT NULL,
+	first text NOT NULL,
+	last text NOT NULL,
+	school text NOT NULL,
+	state text NOT NULL
+);
+
+
+ALTER TABLE schools OWNER TO postgres;
 
 --
 -- Data for Name: users; Type: TABLE DATA; Schema: public; Owner: postgres
 --
 
-COPY users (email, org, username, password, tfa, user_data) FROM stdin;
-eric@sermodigital.com	SermoDigital	admin	$2a$10$YX6/o2EpdDtfNphzkw7tzO/tbq9sD5c.dEqBYnymwplDx4v7u28G2	 f	\N
+COPY users (email, school, username, password) FROM stdin;
+eric@eriscottlagergren@gmail.com	Pierce College	admin	$2a$10$YX6/o2EpdDtfNphzkw7tzO/tbq9sD5c.dEqBYnymwplDx4v7u28G2
 \.
 
 --
--- Data for Name: orgs; Type: TABLE DATA; Schema: public; Owner: postgres
+-- Data for Name: schools; Type: TABLE DATA; Schema: public; Owner: postgres
 --
 
-COPY orgs (org, prefix) FROM stdin;
-SermoDigital	sd_
+COPY schools (school) FROM stdin;
+Pierce College
 \.
-
---
--- Name: failed_logins_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres; Tablespace: 
---
-
-ALTER TABLE ONLY failed_logins
-	ADD CONSTRAINT failed_logins_pkey PRIMARY KEY (ip);
-
-
---
--- Name: hotp_codes_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres; Tablespace: 
---
-
-ALTER TABLE ONLY hotp_codes
-	ADD CONSTRAINT hotp_codes_pkey PRIMARY KEY (email);
-
-
---
--- Name: jobs_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres; Tablespace: 
---
-
-ALTER TABLE ONLY jobs
-	ADD CONSTRAINT jobs_pkey PRIMARY KEY (job_id);
-
 
 --
 -- Name: users_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres; Tablespace: 
@@ -162,13 +108,11 @@ ALTER TABLE ONLY users
 	ADD CONSTRAINT users_pkey PRIMARY KEY (email);
 
 --
--- Name: orgs_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres; Tablespace: 
+-- Name: schools_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres; Tablespace: 
 --
 
-ALTER TABLE ONLY orgs
-	ADD CONSTRAINT orgs_pkey PRIMARY KEY (org);
-
-
+ALTER TABLE ONLY schools
+	ADD CONSTRAINT schools_pkey PRIMARY KEY (school);
 
 --
 -- Name: public; Type: ACL; Schema: -; Owner: postgres
